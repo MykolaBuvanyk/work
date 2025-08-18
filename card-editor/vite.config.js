b7c21ccd1bd03cd23d95462be947599484413728
Mykola Buvanyk <mykola.buvanyk@lnu.edu.ua>
2025-08-18 14:28:46 +0300

    Update canvas margin and enhance adaptive shape handling in Toolbar

--- card-editor/vite.config.js
+++ card-editor/vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
     include: ['fabric'],
   },
   server: {
-    allowedHosts: ['305dce6666b4.ngrok-free.app'],
+    allowedHosts: ['2b0edf83ed45.ngrok-free.app'],
   },
 })
